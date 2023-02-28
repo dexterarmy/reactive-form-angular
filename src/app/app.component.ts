976f8bfb091a73c6892c31839fcb6908bbc540d1
dexterarmy <immohitsharma8@gmail.com>
2023-02-28 12:38:17 +0530

    template driven form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-weather';
+  defaultCountry = 'india';
+  gender = [
+    {id: '1', value: 'Male'},
+    {id: '2', value: 'Female'},
+    {id: '3', value: 'other'}
+  ];
+  defaultGender = 'Male';
+  firstName : string ;
+  lastName: string ;
+  email: string ;
+  gen: string ;
+  country: string ;
+  // form property storing ngForm object
+  @ViewChild('myForm') form: NgForm  ;
+
+  onSubmit(){
+    console.log(this.form);
+    this.firstName = this.form.value.personDetails.firstname;
+
+    // this.form.reset();
+  }
+
+  setDefaultValues(){
+    // this.form.value.personDetails.firstName = 'Mohit';
+    // this.form.setValue({
+    //   personDetails: {
+    //     firstName : 'Mohit'
+    //   }
+    // })
+    this.form.form.patchValue({
+      personDetails: {
+        firstname : 'Mohit',
+        lastname: 'sharma',
+        email: 'abc@gmail.com'
+        
+      
+      }
+    });
+  }
 }
